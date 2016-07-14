e9393dd4d20f4b29092c5aac882130eee2f496de
vinay <jami.vinaysagar@gmail.com>
2016-07-14 14:59:50 +0530

    updating test sources

---
+++ unit_testing/src/gcov.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int  main(void)
+{
+    int i, total;
+
+
+    total = 0;
+
+
+    for (i = 0; i < 10; i++)
+        total += i;
+
+
+    if (total != 45)
+        printf ("Failure\n");
+    else
+        printf ("Success\n");
+
+    return 0;
+}
