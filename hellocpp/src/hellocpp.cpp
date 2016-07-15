18a877b4248c095aefee1fc88b303e317863b194
vinay <jami.vinaysagar@gmail.com>
2016-07-15 10:35:10 +0530

    new project

---
+++ hellocpp/src/hellocpp.cpp
@@ -0,0 +1,24 @@
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+
+class Message
+{
+  public:
+
+    void display() {
+
+      cout << "Hello Vinay sagar";
+    }
+};
+
+int main()
+{
+	Message classobj;
+	classobj.display();
+	return 0;
+}
